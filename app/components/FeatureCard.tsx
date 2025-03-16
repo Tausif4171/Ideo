93db7be491094cfedd0f8731db60e49dea7f122d
Tausif4171 <tausifkhan4171@gmail.com>
2025-03-16 18:05:00 +0530

    folder stucture setup ✨

---
+++ app/components/FeatureCard.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const FeatureCard = () => {
+  return <div>FeatureCard</div>;
+};
---
+++ app/components/Ideas/IdeaSection.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const IdeaSection = () => {
+  return <div>IdeaSection</div>;
+};
