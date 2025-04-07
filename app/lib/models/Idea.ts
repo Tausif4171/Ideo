c727360e5da50c0bef01c59217d912f5713b683c
Tausif4171 <tausifkhan4171@gmail.com>
2025-04-07 20:03:50 +0530

    Idea schema created ✅

---
+++ app/lib/models/Idea.ts
@@ -0,0 +1,9 @@
+import mongoose, { Schema, model, models } from "mongoose";
+
+const IdeaSchema = new Schema({
+  text: { type: String, required: true },
+  favorite: { type: Boolean, default: false },
+  done: { type: Boolean, default: false },
+});
+
+export const Idea = models.Idea || model("Idea", IdeaSchema);
