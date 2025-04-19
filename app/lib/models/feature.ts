43296aced8cb38fcf553cedcdd8c90d2e452e755
Tausif4171 <tausifkhan4171@gmail.com>
2025-04-19 12:24:58 +0530

    Rename featureSchema to FeatureSchema for consistency in naming conventions

--- app/lib/models/feature.ts
+++ app/lib/models/feature.ts
@@ -1,9 +1,9 @@
 import {Schema, model, models} from "mongoose";
 
-const featureSchema = new Schema({
+const FeatureSchema = new Schema({
     text:{type:String,required:true},
     favorite:{type:Boolean,default:false},
     done:{type:Boolean,default:false},
 })
 
-export const Feature = models.Feature || model("Feature", featureSchema); 
+export const Feature = models.Feature || model("Feature", FeatureSchema); 
