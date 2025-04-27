9848dd700d8f30f8c5fa6a60aeafd2173df8b8d4
Tausif4171 <tausifkhan4171@gmail.com>
2025-04-27 13:44:32 +0530

    update routes

--- app/api/features/[id]/route.ts
+++ app/api/features/[id]/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { connectToDB } from "@/app/lib/mongodb";
-import { Feature } from "@/app/lib/models/Feature";
+import { Feature } from "../../../lib/models/Feature";
 
 // Updated PATCH handler
 export async function PATCH(
--- app/lib/models/Idea.ts
+++ app/lib/models/Idea.ts
@@ -1,4 +1,4 @@
-import mongoose, { Schema, model, models } from "mongoose";
+import  { Schema, model, models } from "mongoose";
 
 const IdeaSchema = new Schema({
   text: { type: String, required: true },
